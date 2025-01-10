33dea59e0558b53454aae84f1cd8a549e9dde0cd
Mirko <mirkocaccavone03@gmail.com>
2025-01-10 20:52:04 +0100

    [fix] change class .lamp

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 // Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
-
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
 // selezione degli elementi
 const bottone = document.querySelector('.bottone');
-const img = document.querySelector('.off');
+const img = document.querySelector('.lamp');
 
 // gestione dell'evento
 bottone.addEventListener('click', function(){
