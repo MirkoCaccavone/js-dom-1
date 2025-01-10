9c4a7221626870b7e000a4d30316593a5c838ab6
Mirko <mirkocaccavone03@gmail.com>
2025-01-10 19:13:11 +0100

    [feat] add function in js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+
+// Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+// selezione degli elementi
+const bottone = document.querySelector('.bottone');
+const img = document.querySelector('.off');
+
+// gestione dell'evento
+bottone.addEventListener('click', function(){
+    img.src = './img/yellow_lamp.png';
+    img.classList.remove('off');
+    img.classList.add('on');
+});
+
+
+
+
+
+
+
+
+
